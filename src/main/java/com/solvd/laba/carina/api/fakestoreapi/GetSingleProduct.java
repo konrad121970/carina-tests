90300441cecc781f15b5fd9a5c5a246ac2781c88
konrad121970 <konrad121970@windowslive.com>
2024-02-04 23:29:06 +0100

    Update response schema and load json into POJO

--- src/main/java/com/solvd/laba/carina/api/fakestoreapi/GetSingleProduct.java
+++ src/main/java/com/solvd/laba/carina/api/fakestoreapi/GetSingleProduct.java
@@ -3,11 +3,14 @@ package com.solvd.laba.carina.api.fakestoreapi;
 import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
 import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
 import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
 
 @Endpoint(url = "${config.fakestore_api_url}/products/${productId}", methodType = HttpMethodType.GET)
 @ResponseTemplatePath(path = "api/fakestore/get_single_product.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetSingleProduct extends AbstractApiMethodV2 {
 
     public GetSingleProduct(int productId) {
--- src/test/java/com/solvd/laba/carina/api/fakestoreapi/ProductTest.java
+++ src/test/java/com/solvd/laba/carina/api/fakestoreapi/ProductTest.java
@@ -1,6 +1,9 @@
 package com.solvd.laba.carina.api.fakestoreapi;
 
 import com.solvd.laba.carina.api.fakestoreapi.domain.Product;
+import com.zebrunner.agent.core.registrar.domain.ObjectMapperImpl;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import kong.unirest.ObjectMapper;
 import org.testng.annotations.Test;
 
 public class ProductTest {
@@ -10,12 +13,18 @@ public class ProductTest {
 
         Product product = new Product();
         product.setId(1);
+        //product.setPrice(120.00);
 
         GetSingleProduct getSingleProduct = new GetSingleProduct(product.getId());
         getSingleProduct.addProperty("product", product);
 
-        getSingleProduct.callAPI();
+        //getSingleProduct.expectResponseStatus(HttpResponseStatusType.OK_200);
+        String response = getSingleProduct.callAPIExpectSuccess().asString();
+        ObjectMapper objectMapper = new ObjectMapperImpl();
+
+        Product product1 = objectMapper.readValue(response, Product.class);
 
         getSingleProduct.validateResponse();
+
     }
 }
