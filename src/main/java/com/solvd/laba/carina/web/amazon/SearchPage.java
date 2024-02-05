cced318ba5c971a1f98fc90491480e25ab49bd36
konrad121970 <konrad121970@windowslive.com>
2024-02-06 00:43:46 +0100

    Add SearchPage class

---
+++ src/main/java/com/solvd/laba/carina/web/amazon/SearchPage.java
@@ -0,0 +1,22 @@
+package com.solvd.laba.carina.web.amazon;
+
+import com.solvd.laba.carina.web.amazon.components.ProductCard;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchPage extends AbstractPage {
+
+    @FindBy(xpath = "//*[contains(@class, 's-card-container')]")
+    private List<ProductCard> cards;
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public List<ProductCard> getCards() {
+        return cards;
+    }
+}
